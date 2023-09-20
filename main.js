e4c99460bd90a4f8267d7d66a53710e6d69e85b8
“leidert11” <110786757+leidert11@users.noreply.github.com>
2023-09-20 12:39:46 -0500

    feat: @campusland

---
+++ main.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const formulario = document.querySelector("form");
+    const tabla = document.querySelector("#myData");
+    const API_URL = "https://6509e17df6553137159c2ff5.mockapi.io/tabla";
+    let isEditing = false;
+
+    async function renderData(){
+        const res = await fetch(API_URL);
+        const data = await res.json();
+
+        tabla.innerHTML = "";
+        data.forEach((item) => {
+            tabla.innerHTML +=
+            `
+            <tr>
+                <td>${item.id}</td>
+                <td>${item.valor}</td>
+                <td>${item.caja}</td>
+                <td><button class="editar" data-id="${item.id}" >Editar</button></td>
+                <td><button class="eliminar" data-id="${item.id}" >Eliminar</button></td>
+            </tr>
+            `;
+        });
+    }
+    renderData();
+
+});
